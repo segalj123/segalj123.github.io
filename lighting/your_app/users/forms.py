2aec9daae9a07b61b7f51c05881b31ee011a48fb
segalj123 <145132531+segalj123@users.noreply.github.com>
2024-06-26 15:26:22 -0400

    require update

--- lighting/your_app/users/forms.py
+++ lighting/your_app/users/forms.py
@@ -41,3 +41,4 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
+    
