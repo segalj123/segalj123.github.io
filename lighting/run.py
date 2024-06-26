14a36cb847e321873e291a31d89e5e9a362bb86d
segalj123 <145132531+segalj123@users.noreply.github.com>
2024-06-26 15:19:12 -0400

    config

--- lighting/run.py
+++ lighting/run.py
@@ -1,20 +1,6 @@
 from your_app import create_app
-from your_app.initial_data import add_initial_data
-from flask.cli import AppGroup
 
 app = create_app()
 
-# Create a custom CLI command
-cli = AppGroup('custom')
-
-@cli.command('add-initial-data')
-def add_initial_data_command():
-    """Add initial data to the database."""
-    with app.app_context():
-        add_initial_data()
-
-# Register the custom CLI command
-app.cli.add_command(cli)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- lighting/your_app/__init__.py
+++ lighting/your_app/__init__.py
@@ -2,26 +2,30 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from your_app.config import Config  # Import the config here
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object(config_class)
 
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     from your_app.users.routes import users
     from your_app.main.routes import main
+    from your_app.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
