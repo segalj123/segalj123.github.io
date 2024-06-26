f9c5f5b6d558b7e6a1da46083ae41ff646bb1a44
segalj123 <145132531+segalj123@users.noreply.github.com>
2024-06-26 15:35:51 -0400

    nm

--- lighting/your_app/__init__.py
+++ lighting/your_app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from your_app.config import Config  # Import the config here
+from your_app.config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
