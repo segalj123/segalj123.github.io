13bcf6655950d15350b07810a04da78bdd80d299
segalj123 <145132531+segalj123@users.noreply.github.com>
2024-07-05 14:20:37 -0400

    hope

--- new/app.py
+++ new/app.py
@@ -59,6 +59,10 @@ def swipe_action():
     db.session.commit()
     return jsonify({'status': 'success'})
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
